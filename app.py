b01eff9a7fd63c374cd9227ee71eedb0acb84284
randa-ama <ampahranda03@gmail.com>
2025-12-12 02:26:27 -0500

    fix app

--- app.py
+++ app.py
@@ -257,6 +257,7 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = Dash(
     __name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True
 )
+server = app.server
 
 # --------------------------
 # App Layout
